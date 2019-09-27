ac503c94357e2f9eadd6d392f1fbe393e054745a
Anthony Ho <anthonyho007@gmail.com>
2019-09-26 23:59:11 -0400

    add k fold validation + results

--- models/five_fold_validation.py
+++ models/five_fold_validation.py
@@ -1,41 +1,28 @@
-from logistic_regression import LogisticRegression
 import pandas as pd
 import numpy as np
+# from linear_discriminant_analysis import LearnDiscriminantAnalysis as LDA
+# from logistic_regression import LogisticRegression
+from sklearn.model_selection import KFold
 
+def k_fold_validation(X, y, classifier, n_fold):
+    N = len(y)
+    score = 1
+    kf = KFold(n_splits=n_fold)
+    for train_i, test_i in kf.split(X):
+        X_train, X_test = X[train_i], X[test_i]
+        y_train, y_test = y[train_i], y[test_i]
+        classifier.fit(X_train, y_train)
+        s = classifier.score(X_test, y_test)
+        if s < score:
+            score = s
 
-def validate_logistic_regression_for_wine_quality():
-    num_of_folds = 5
-    learning_rate= 0.000001
-    max_iterations = 100
-    df = pd.read_csv("../data/winequality/winequality-red.csv", sep=";")
-    df['classified']=[1 if x>=6 else 0 for x in df["quality"] ]
-    fold_size = int(round(df.shape[0]/num_of_folds))
-    for i in range(num_of_folds):
-        x_test = df[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']][i*fold_size:fold_size+i*fold_size]
-        x_train_part_1 = df[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']][fold_size+i*fold_size:]
-        x_train_part_2 = df[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']][:i*fold_size]
-        x_train = x_train_part_1.append(x_train_part_2)
-        print(x_train)
-        print(x_test)
-
-
-        y_test = df[['classified']][i*fold_size:fold_size+i*fold_size]
-        y_train_part_1 = df[['classified']][fold_size+i*fold_size:]
-        y_train_part_2 = df[['classified']][:i*fold_size]
-        y_train = y_train_part_1.append(y_train_part_2)
-        print(y_train)
-        print(y_test)
-
-        model = LogisticRegression()
-        model.fit(learning_rate, max_iterations, np.array(x_train), np.array(y_train))
-        y_pred = model.predict(np.array(x_test))
-        print(y_pred)
-        print("score", model.score(np.array(x_test), np.array(y_test)))
-    pass
-
-
+    return score
 
 if __name__ == "__main__":
-    validate_logistic_regression_for_wine_quality()
-    pass
-
+    df = pd.read_csv("data/winequality/winequality-red.csv", sep=";")
+    df['classified']=[1 if x>=6 else 0 for x in df["quality"] ]
+    X = df[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']]
+    y = df[['classified']]
+    model =LogisticRegression(0.00001, 100)
+    score = k_fold_validation(np.array(X), np.array(y), model, 5)
+    print("score", score)
--- models/linear_discriminant_analysis.py
+++ models/linear_discriminant_analysis.py
@@ -3,7 +3,7 @@ import math
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from utils import evaluate_acc
+from .utils import evaluate_acc
 
 class LearnDiscriminantAnalysis:
     def __init__(self):
@@ -13,7 +13,7 @@ class LearnDiscriminantAnalysis:
         self.w = None
 
     def fit(self, X, y):
-        X1,X0 = X[y == 1], X[y == 0]
+        X1,X0 = X[y[:,0] == 1], X[y[:,0] == 0]
 
         N1,N0 = len(X1), len(X0)
 
@@ -24,24 +24,24 @@ class LearnDiscriminantAnalysis:
         plog = math.log(P1/P0)
         #find mean
         mc1 = X1.mean(0)
-        mc0 = X0.mean()
+        mc0 = X0.mean(0)
 
         n_feat = len(mc1)
 
-        m1 = mc1.values.reshape(n_feat, 1)
-        m0 = mc0.values.reshape(n_feat, 1)
+        m1 = mc1.reshape(n_feat, 1)
+        m0 = mc0.reshape(n_feat, 1)
         self.m1, self.m0 = m1, m0
 
         #find co-variance
         co_var = np.zeros((n_feat, n_feat))
 
-        for _, row in X1.iterrows():
-            x = row.values.reshape(n_feat, 1)
+        for row in X1:
+            x = row.reshape(n_feat, 1)
             m = m1
             co_var += (x - m).dot((x - m).T / (N1 + N0 - 2))
 
-        for _, row in X0.iterrows():
-            x = row.values.reshape(n_feat, 1)
+        for row in X0:
+            x = row.reshape(n_feat, 1)
             m = m0
             co_var += (x - m).dot((x - m).T) / (N1 + N0 - 2)
 
@@ -52,21 +52,12 @@ class LearnDiscriminantAnalysis:
 
     def predict(self, X):
         pred = []
-        for ind, row in X.iterrows():
+        for row in X:
             ratio = self.w + ((row.T).dot(np.linalg.inv(self.co_var))).dot(self.m1 - self.m0)
             res = 1 if ratio >= 0 else 0
-            pred.append(res)
-        return pred
+            pred.append([res])
+        return np.array(pred)
 
     def score(self, X, y_test):
         y_pred = self.predict(X)
         return evaluate_acc(y_test, y_pred)
-
-if __name__ == "__main__":
-    df = pd.read_csv("data/winequality/winequality-red.csv", sep=";")
-    df['classified']=[1 if x>=6 else 0 for x in df["quality"] ]
-    X_train, X_test, y_train, y_test= train_test_split(df[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']], df.classified, train_size=0.9)
-    model = LearnDiscriminantAnalysis()
-    model.fit(X_train, y_train)
-    score = model.score(X_test, y_test)
-    print("score", score)
